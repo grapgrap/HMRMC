376edc17a54f17090dabec7647455f59deab6525
grapgrap <fussaep@gmail.com>
2017-12-11 19:02:05 +0900

    change project init

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info.component.scss']
 })
 export class InfoComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
