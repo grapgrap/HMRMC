3a286c4ae637f9dc223efc69a743610c2be83004
grapgrap <fussaep@gmail.com>
2017-12-12 15:21:20 +0900

    layout major

--- src/app/main/major/major.component.ts
+++ src/app/main/major/major.component.ts
@@ -3,13 +3,65 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-major',
   templateUrl: './major.component.html',
-  styleUrls: ['./major.component.scss']
+  styleUrls: [ './major.component.scss' ]
 })
 export class MajorComponent implements OnInit {
+  mock: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.mock = [
+      {
+        first: {
+          required: [
+            {name: '대학수학1', credit: 3, design_credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, design_credit: '', is_complete: false}
+          ],
+          electives: [
+            {name: '대학수학1', credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, is_complete: false},
+            {name: '일반물리학2', credit: 3, is_complete: false}
+          ]
+        },
+        second: {
+          required: [
+            {name: '대학수학1', credit: 3, design_credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, design_credit: '', is_complete: false}
+          ],
+          electives: [
+            {name: '대학수학1', credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, is_complete: false},
+            {name: '일반물리학2', credit: 3, is_complete: false}
+          ]
+        }
+      },
+      {
+        first: {
+          required: [
+            {name: '대학수학1', credit: 3, design_credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, design_credit: '', is_complete: false}
+          ],
+          electives: [
+            {name: '대학수학1', credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, is_complete: false},
+            {name: '일반물리학2', credit: 3, is_complete: false}
+          ]
+        },
+        second: {
+          required: [
+            {name: '대학수학1', credit: 3, design_credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, design_credit: '', is_complete: false}
+          ],
+          electives: [
+            {name: '대학수학1', credit: 3, is_complete: true},
+            {name: '일반물리학1', credit: 3, is_complete: false},
+            {name: '일반물리학2', credit: 3, is_complete: false}
+          ]
+        }
+      },
+    ];
   }
 
 }
